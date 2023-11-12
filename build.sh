f94f3b8d2dcc830c4868453626ebc79cdbf68c50
Manojkumar Subramaniam <manoj@electrolance.com>
2023-11-12 15:08:54 +0000

    build.sh: menu pulls the right config and preserve new config

--- build.sh
+++ build.sh
@@ -8,19 +8,14 @@ BUILDROOT_DIR=~/buildroot
 PROJECT_DIR=$(pwd)
 
 # Set the name of your defconfig file
-DEFCONFIG_FILE=raspberrypi3_64_defconfig
+DEFCONFIG_FILE=xraspberrypi3_64_defconfig
 
-# Create a build directory
-mkdir -p $PROJECT_DIR/build
-
-# Change to the build directory
-cd $PROJECT_DIR/build
 
 export BRI=$PROJECT_DIR/build/images
 # Configure the build
 if [ "$1" == "build" ]; then
-    make -C $BUILDROOT_DIR O=$PROJECT_DIR/build BR2_EXTERNAL=$PROJECT_DIR $DEFCONFIG_FILE
-    make -C $BUILDROOT_DIR O=$PROJECT_DIR/build BR2_EXTERNAL=$PROJECT_DIR -j8
+    make O=$PROJECT_DIR BR2_EXTERNAL=$PROJECT_DIR -C $BUILDROOT_DIR 
+    make -j8
 elif [ "$1" == "run" ]; then
     qemu-system-aarch64 -machine raspi3b -cpu cortex-a72 -nographic -dtb $BRI/bcm2710-rpi-3-b-plus.dtb -m 1G -smp 4 -kernel $BRI/Image -sd $BRI/sdcard.img \
     -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1"
@@ -28,6 +23,14 @@ elif [ "$1" == "run" ]; then
 elif [ "$1" == "defsize" ]; then
     echo "Resizing sdcard.img to 8G"
     qemu-img resize $BRI/sdcard.img 8G
+elif [ "$1" == "menu" ]; then
+    echo "Running menuconfig"
+    BR2_DEFCONFIG=$PROJECT_DIR
+    if [ -f $PROJECT_DIR/build/.config ]; then
+        BR2_DEFCONFIG=$PROJECT_DIR/build/.config
+    fi
+    make O=$PROJECT_DIR/build BR2_EXTERNAL=$PROJECT_DIR -C $BUILDROOT_DIR $BR2_DEFCONFIG menuconfig
+    
 else
     echo "Invalid argument. Usage: ./build.sh [build|run|defsize]"
 fi
