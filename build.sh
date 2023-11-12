c6a25a59c6dd9f541f9610c2fa70ccb3f723c78b
Manojkumar Subramaniam <manoj@electrolance.com>
2023-11-12 20:03:32 +0000

    build.sh: fix build with .config

--- build.sh
+++ build.sh
@@ -10,11 +10,29 @@ PROJECT_DIR=$(pwd)
 # Set the name of your defconfig file
 DEFCONFIG_FILE=xraspberrypi3_64_defconfig
 
+configure_build() {
+    if [ -f $PROJECT_DIR/build/.config ]; then
+        BR2_DEFCONFIG=$PROJECT_DIR/build/.config
+        return 0 # success
+    else
+        return 1 # failure
+    fi
+}
 
-export BRI=$PROJECT_DIR/build/images
+
+export BRI=$PROJECT_DIR/images
 # Configure the build
 if [ "$1" == "build" ]; then
-    make O=$PROJECT_DIR BR2_EXTERNAL=$PROJECT_DIR -C $BUILDROOT_DIR 
+
+    configure_build
+    if [ $? -eq 0 ]; then
+        make O=$PROJECT_DIR/build BR2_EXTERNAL=$PROJECT_DIR -C $BUILDROOT_DIR 
+        make -j8
+    else
+        echo "No .config file found. Run ./build.sh menu to configure the build"
+        exit 1
+    fi
+    make O=$PROJECT_DIR BR2_EXTERNAL=$PROJECT_DIR  -C $BUILDROOT_DIR 
     make -j8
 elif [ "$1" == "run" ]; then
     qemu-system-aarch64 -machine raspi3b -cpu cortex-a72 -nographic -dtb $BRI/bcm2710-rpi-3-b-plus.dtb -m 1G -smp 4 -kernel $BRI/Image -sd $BRI/sdcard.img \
@@ -25,11 +43,12 @@ elif [ "$1" == "defsize" ]; then
     qemu-img resize $BRI/sdcard.img 8G
 elif [ "$1" == "menu" ]; then
     echo "Running menuconfig"
-    BR2_DEFCONFIG=$PROJECT_DIR
+    BR2_DEFCONFIG=$DEFCONFIG_FILE
     if [ -f $PROJECT_DIR/build/.config ]; then
         BR2_DEFCONFIG=$PROJECT_DIR/build/.config
+        echo "Using existing .config file"
     fi
-    make O=$PROJECT_DIR/build BR2_EXTERNAL=$PROJECT_DIR -C $BUILDROOT_DIR $BR2_DEFCONFIG menuconfig
+    make O=$PROJECT_DIR/build BR2_EXTERNAL=$PROJECT_DIR $BR2_DEFCONFIG  -C $BUILDROOT_DIR menuconfig
     
 else
     echo "Invalid argument. Usage: ./build.sh [build|run|defsize]"
